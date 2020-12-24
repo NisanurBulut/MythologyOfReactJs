49ae7855c0fbe271f933f6070ba275e8b5b91b21
NisanurBulut <nbulut@sarplojistik.com>
2020-12-24 15:06:59 +0300

    refactoring navbar

--- arena/src/layouts/Navbar.js
+++ arena/src/layouts/Navbar.js
@@ -2,15 +2,15 @@ import React from 'react';
 import PropTypes from "prop-types";
 import {Link} from 'react-router-dom';
 
-function Navbar(props) {
+function Navbar({title}) {
   return (
     <nav className="navbar navbar-expand-md bg-dark navbar-dark">
        <div className="container-fluid">
-         <Link to="/" className="navbar-brand">{props.title}</Link>
+         <Link to="/" className="navbar-brand">{title}</Link>
      <div className="justify-content-end">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <Link className="nav-link" to="/">Home</Link>
+          <Link className="nav-link active" to="/">Home</Link>
         </li>
         <li className="nav-item">
         <Link className="nav-link" to="/add">Add</Link>
