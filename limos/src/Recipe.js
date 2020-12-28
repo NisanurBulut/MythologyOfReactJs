3e9280c3f2d9ef349736dffbd2236b412d3e6010
NisanurBulut <nbulut@sarplojistik.com>
2020-12-28 19:44:57 +0300

    implemented recipe functional component

---
+++ limos/src/Recipe.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+const Recipe =()=> {
+
+        return (
+            <div>
+                <h1>Title</h1>
+                <p>Calories</p>
+                <img src='' />
+            </div>
+        )
+
+}
+
+export default Recipe;
