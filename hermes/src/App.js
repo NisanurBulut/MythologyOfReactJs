d4079da961c5a5cc838b885a35109335538cf472
NisanurBulut <nbulut@sarplojistik.com>
2020-12-28 11:23:29 +0300

    implemented review

--- hermes/src/App.js
+++ hermes/src/App.js
@@ -1,24 +1,22 @@
+import React from 'react';
+import Review from './Review';
+import {FaChessBoard} from 'react-icons/fa';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<section className="main">
+  <container>
+    <div className="title">
+      <h2>Hermes</h2>
+      <div className="underline"></div>
     </div>
+    <Review/>
+  </container>
+</section>
+
   );
 }
 
---
+++ hermes/src/Review.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const Review=()=> {
+
+        return (
+            <h2>Chess Piece</h2>
+        )
+
+}
+
+export default Review;
