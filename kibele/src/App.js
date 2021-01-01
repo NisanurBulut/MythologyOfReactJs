a4d10594add3c3de11b497af96a58f04f9d25a75
NisanurBulut <nbulut@sarplojistik.com>
2021-01-01 15:59:28 +0300

    implemented filtering for category

--- kibele/src/App.js
+++ kibele/src/App.js
@@ -3,17 +3,27 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all',...new Set(items.map((item)=>item.category))];
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if(category==='all'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems = menuItems.filter((item)=> item.category===category);
+    setMenuItems(newItems);
+  };
   return (
     <main>
       <section className="menu section">
         <div className="title">
           <h2>Kibele's Menu</h2>
-        <div className="underline"></div>
+          <div className="underline"></div>
         </div>
-        <Categories/>
+        <Categories categories={allCategories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
