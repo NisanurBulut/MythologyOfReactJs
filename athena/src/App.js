4cf8673a635771de16c61970fa35e212758693c8
NisanurBulut <nbulut@sarplojistik.com>
2021-01-01 19:46:58 +0300

    created athena project

---
+++ athena/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+
+const url = ' http://localhost:3000/jobs';
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs]=useState([]);
+  const [value, setValue]=useState(0);
+  const fetchJobs = async ()=>{
+    const response = await fetch(url);
+    var newJobs = response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
+  useEffect(()=>{
+    fetchJobs()
+  },[])
+  return <h2>athena project setup</h2>;
+}
+
+export default App;
