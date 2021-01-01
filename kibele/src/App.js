5b9e2096696a6df128819e68f8e25426c72ad88b
NisanurBulut <nbulut@sarplojistik.com>
2021-01-01 15:10:03 +0300

    created kibele project

---
+++ kibele/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  return <h2>Kibele project setup</h2>;
+}
+
+export default App;
