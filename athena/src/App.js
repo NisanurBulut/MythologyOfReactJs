a14149acf455588f29d7e98af06678be527f86a0
NisanurBulut <nbulut@sarplojistik.com>
2021-01-02 13:59:39 +0300

    implemented data for athena

--- athena/src/App.js
+++ athena/src/App.js
@@ -25,31 +25,32 @@ function App() {
       </section>
     );
   }
-  const { company, dates, duties, title } = jobs[value];
+  const {dates, duties, title } = jobs[value];
   return (
     <section className="section">
-      <div className="title">
-        <h2>Athena</h2>
-        <div className="underline"></div>
-      </div>
-      <div className="jobs-center">
-        <div className="btn-container">
+          <div className="btn-container">
           {jobs.map((job,index)=>{
             return <button
             key={job.id}
             onClick={()=>setValue(index)}
             className={`job-btn ${index===index && 'active-btn'}`}>
-              {
-               job.company
-              }
+              <b>{job.title}</b>
             </button>
           })
           }
         </div>
+      <div className="title">
+        {/* <h2>Athena</h2>
+        <div className="underline"></div> */}
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+
+        </div>
         {/* job info */}
         <article className="job-info">
-          <h3>{title}</h3>
-          <h4>{company}</h4>
+          <h4>{title}</h4>
+
           <p className="job-date">{dates}</p>
           {
             duties.map((duty,index)=>{
