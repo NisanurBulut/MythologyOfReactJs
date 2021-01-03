fcf88d83350196e77c41bb9091026edbbf4ca637
NisanurBulut <nbulut@sarplojistik.com>
2021-01-03 15:38:38 +0300

    implemented slider

--- dionysos/src/App.js
+++ dionysos/src/App.js
@@ -1,9 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>Dionysos project setup</h2>;
+  const [wines, setWines]=useState(data);
+  const [index, setIndex]=useState(0);
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span> Wines
+        </h2>
+      </div>
+      <div className="section-center">
+        {wines.map((wine,wineIndex)=>{
+          const {id,image,name,title,quote}=wine;
+          return <article key={id}>
+            <img src={image} alt={name} className="wine-img"/>
+            <h4>{name}</h4>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon"></FaQuoteRight>
+          </article>
+        })}
+        <FaChevronLeft className="prev"></FaChevronLeft>
+        <FaChevronRight className="next"></FaChevronRight>
+      </div>
+    </section>
+  );
 }
 
 export default App;
--- dionysos/src/data.js
+++ dionysos/src/data.js
@@ -1,114 +1,114 @@
 const wines = [
   {
     id: 1,
-    image: "https://www.edamam.com/web-img/457/457eea40729d786bcec111309e2f2702.jpg",
-    name: "Peach Leaf Wine",
-    title: "David Lebovitz",
-    quote:"40 x peach leaves, unsprayed"+
-    "750 ml fruity red wine"+
-    "3 tbsp Cognac or brandy"+
-    "7 tbsp sugar"
+    image: 'https://www.edamam.com/web-img/457/457eea40729d786bcec111309e2f2702.jpg',
+    name: 'Peach Leaf Wine',
+    title: 'David Lebovitz',
+    quote:'40 x peach leaves, unsprayed'+
+    '750 ml fruity red wine'+
+    '3 tbsp Cognac or brandy'+
+    '7 tbsp sugar'
   },
   {
     id: 2,
-    image: "https://www.edamam.com/web-img/203/203da50dcb6d073a7ee5e5b989f19af4.jpg",
-    name: "What to Do With Leftover Wine: Mulled Wine Syrup Recipe",
-    title: "Serious Eats",
-    quote:"1 bottle red wine"+
-    "1 cup sugar"+
-    "1 stick cinnamon"+
-    "5 cloves"
+    image: 'https://www.edamam.com/web-img/203/203da50dcb6d073a7ee5e5b989f19af4.jpg',
+    name: 'What to Do With Leftover Wine: Mulled Wine Syrup Recipe',
+    title: 'Serious Eats',
+    quote:'1 bottle red wine'+
+    '1 cup sugar'+
+    '1 stick cinnamon'+
+    '5 cloves'
     },
   {
     id: 3,
-    image:"https://www.edamam.com/web-img/c36/c369b9e05899c07a611501bcbd874723.jpg",
-    name: "Wine Spritzer",
-    title:  "Bon Appetit",
-    quote:"3 oz. aromatic white wine (such as Sauvignon Blanc, Riesling, or Gewürztraminer)"+
-    "1 oz. club soda"+
-    "dash of orange or lemon bitters"+
-    "1 Orange or lemon twist"
+    image:'https://www.edamam.com/web-img/c36/c369b9e05899c07a611501bcbd874723.jpg',
+    name: 'Wine Spritzer',
+    title:  'Bon Appetit',
+    quote:'3 oz. aromatic white wine (such as Sauvignon Blanc, Riesling, or Gewürztraminer)'+
+    '1 oz. club soda'+
+    'dash of orange or lemon bitters'+
+    '1 Orange or lemon twist'
   },
   {
     id: 4,
-    image: "https://www.edamam.com/web-img/c60/c600e22012d65a926c055774a8a12996.jpg",
-    name: "Oranges in Red Wine",
-    title: "Epicurious",
-    quote: "6 large navel oranges, peeled and coarsely chopped, discarding tough membranes"+
-    "1 (750-ml) bottle Chianti or other Italian red wine"
+    image: 'https://www.edamam.com/web-img/c60/c600e22012d65a926c055774a8a12996.jpg',
+    name: 'Oranges in Red Wine',
+    title: 'Epicurious',
+    quote: '6 large navel oranges, peeled and coarsely chopped, discarding tough membranes'+
+    '1 (750-ml) bottle Chianti or other Italian red wine'
   },
   {
     id: 5,
-    image: "https://www.edamam.com/web-img/8b4/8b46f592c913cdbfe01ef2a49f03ad50.jpg",
-    name: "White Wine Granita",
-    title: "Saveur",
-    quote:  "2 cups full-bodied white wine"+
-    "1/2 cup unsweetened apple juice"+
-    "1/3 cup water"+
-    "1/2 cup sugar"+
-    "Dried apple slices, for serving"
+    image: 'https://www.edamam.com/web-img/8b4/8b46f592c913cdbfe01ef2a49f03ad50.jpg',
+    name: 'White Wine Granita',
+    title: 'Saveur',
+    quote:  '2 cups full-bodied white wine'+
+    '1/2 cup unsweetened apple juice'+
+    '1/3 cup water'+
+    '1/2 cup sugar'+
+    'Dried apple slices, for serving'
   },
   {
     id: 6,
-    image: "https://www.edamam.com/web-img/fd8/fd885d3f3af6ff28cbb4ec5a0b9be1c3.jpg",
-    name: "Spiced mulled wine",
-    title:"BBC",
-    quote:"½ bottle red wine"+
-    "1 cinnamon stick"+
-    "2 star anise"+
-    "2 cloves"+
-    "1 orange, sliced"+
-    "1 lemon, sliced"+
-    "50g/1¾oz brown sugar"
+    image: 'https://www.edamam.com/web-img/fd8/fd885d3f3af6ff28cbb4ec5a0b9be1c3.jpg',
+    name: 'Spiced mulled wine',
+    title:'BBC',
+    quote:'½ bottle red wine'+
+    '1 cinnamon stick'+
+    '2 star anise'+
+    '2 cloves'+
+    '1 orange, sliced'+
+    '1 lemon, sliced'+
+    '50g/1¾oz brown sugar'
   },
   {
     id: 7,
-    image: "https://www.edamam.com/web-img/2cc/2ccdc4320289801ef8f577ffe07295e9.jpg",
-    name:   "Red Wine Sauce",
-    title:"Martha Stewart",
-    quote: "1 cup red wine"+
-    "2 tablespoons cold butter, cut up"+
-    "Coarse salt"
+    image: 'https://www.edamam.com/web-img/2cc/2ccdc4320289801ef8f577ffe07295e9.jpg',
+    name:   'Red Wine Sauce',
+    title:'Martha Stewart',
+    quote: '1 cup red wine'+
+    '2 tablespoons cold butter, cut up'+
+    'Coarse salt'
   },
   {
     id: 8,
-    image: "https://www.edamam.com/web-img/005/00536c20cd0edbd552b5d21c836d15c8.jpg",
-    name: "Mulled wine",
-    title:"Jamie Oliver",
-    quote:"200 g caster sugar"+
-    "6 cloves"+
-    "3 star anise"+
-    "1 cinnamon stick"+
-    "2 bay leaves"+
-    "2 oranges"+
-    "1 lemon"+
-    "2 x 750 ml bottles of red wine"+
-    "100 ml crème de cassis"+
-    "1 whole nutmeg , for grating"
+    image: 'https://www.edamam.com/web-img/005/00536c20cd0edbd552b5d21c836d15c8.jpg',
+    name: 'Mulled wine',
+    title:'Jamie Oliver',
+    quote:'200 g caster sugar'+
+    '6 cloves'+
+    '3 star anise'+
+    '1 cinnamon stick'+
+    '2 bay leaves'+
+    '2 oranges'+
+    '1 lemon'+
+    '2 x 750 ml bottles of red wine'+
+    '100 ml crème de cassis'+
+    '1 whole nutmeg , for grating'
   },
   {
     id: 9,
-    image: "https://www.edamam.com/web-img/e9e/e9e00d8e46d29a0ec71dca78559a39fc.jpg",
-    name: "Red Wine Sauce",
-    title:"Fine Cooking",
-    quote: "5 Tbs. unsalted butter"+
-    "2 Tbs. red-wine vinegar"+
-    "4 cups low-salt canned beef broth"+
-    "5 sprigs fresh thyme"+
-    "6 medium shallots, thinly sliced (about 2/3 cup)"+
-    "2 cups full-bodied dry red wine (like a Merlot)"
+    image: 'https://www.edamam.com/web-img/e9e/e9e00d8e46d29a0ec71dca78559a39fc.jpg',
+    name: 'Red Wine Sauce',
+    title:'Fine Cooking',
+    quote: '5 Tbs. unsalted butter'+
+    '2 Tbs. red-wine vinegar'+
+    '4 cups low-salt canned beef broth'+
+    '5 sprigs fresh thyme'+
+    '6 medium shallots, thinly sliced (about 2/3 cup)'+
+    '2 cups full-bodied dry red wine (like a Merlot)'
   },
   {
     id: 10,
-    image:  "https://www.edamam.com/web-img/0af/0afcd5d3195c9294d50760985580a7df.jpg",
-    name: "Wine-Poached Peaches",
-    title:"BBC Good Food",
-    quote:  "4 ripe peaches"+
-    "4.0 tbsp golden caster sugar"+
-    "142.0ml double cream , whipped"+
-    "350.0ml bottle dessert wine"+
-    "a dash grenadine (optional)"+
-    "a small handful pistachio kernels, chopped"
+    image:  'https://www.edamam.com/web-img/0af/0afcd5d3195c9294d50760985580a7df.jpg',
+    name: 'Wine-Poached Peaches',
+    title:'BBC Good Food',
+    quote:  '4 ripe peaches'+
+    '4.0 tbsp golden caster sugar'+
+    '142.0ml double cream , whipped'+
+    '350.0ml bottle dessert wine'+
+    'a dash grenadine (optional)'+
+    'a small handful pistachio kernels, chopped'
   }
 ];
 
