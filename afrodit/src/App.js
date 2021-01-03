2b357fa898a14acf858c2ca8afa9341c58ced4b6
NisanurBulut <nbulut@sarplojistik.com>
2021-01-03 21:15:54 +0300

    implemented afrodit

--- afrodit/src/App.js
+++ afrodit/src/App.js
@@ -4,7 +4,27 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>Afrodit</h2>
+  const [color,setColor]=useState('');
+  const [error, setError]=useState(false);
+  const [list,setList]=useState([]);
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+  }
+  return (
+    <>
+    <section className="container">
+      <h3>Color generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={color} onChange={(e)=>setColor(e.target.value)}/>
+        <button className="btn" type="submit">Generate</button>
+      </form>
+    </section>
+    <section className="colors">
+
+    </section>
+    </>
+  );
 }
 
 export default App
