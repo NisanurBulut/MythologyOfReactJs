57809ae6efc1fb621b0871f6bde4814e9fe43a67
NisanurBulut <nbulut@sarplojistik.com>
2021-01-04 11:22:25 +0300

    implemented setName

--- apollo/src/App.js
+++ apollo/src/App.js
@@ -1,33 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  const [name, setName]=useState('');
-  const [list, setList]=useState([]);
-  const [isEditing,setIsEditing]=useState(false);
-  const [editId,setEditId]=useState(null);
-  const [alert, setAlert]=useState({show:false, msg:'',type:''});
-  const handleSubmit =(e)=>{
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const handleSubmit = (e) => {
     e.preventDefault();
-  }
+  };
   return (
     <section className="section-center">
-      <form action="instrument-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert/>}
+      <form className="instrument-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
         <h3>Apollo</h3>
         <div className="form-control">
-          <input type="text"/>
+          <input
+            type="text"
+            className="instrument"
+            placeholder="e.g. guitar"
+            value={name}
+            onChange={(e)=>setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? 'Edit' : 'Add'}
+          </button>
         </div>
       </form>
       <div className="instrument-container">
-        <List/>
-        <button className="clear-btn">
-          Clear Items
-        </button>
+        <List />
+        <button className="clear-btn">Clear Instruments</button>
       </div>
     </section>
   );
 }
 
-export default App
+export default App;
