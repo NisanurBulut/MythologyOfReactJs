2bf7da0f8337c8b06dca74e2fe668eb79f0b1f3e
NisanurBulut <nbulut@sarplojistik.com>
2021-01-04 11:10:28 +0300

    apollo is installed

--- apollo/src/App.js
+++ apollo/src/App.js
@@ -8,10 +8,20 @@ function App() {
   const [isEditing,setIsEditing]=useState(false);
   const [editId,setEditId]=useState(null);
   const [alert, setAlert]=useState({show:false, msg:'',type:''});
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+  }
   return (
     <section className="section-center">
+      <form action="instrument-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert/>}
+        <h3>Apollo</h3>
+      </form>
       <div className="instrument-container">
-
+        <List/>
+        <button className="clear-btn">
+          Clear Items
+        </button>
       </div>
     </section>
   );
