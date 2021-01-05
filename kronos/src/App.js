3727c88eff511b4755b09aee9bdda79cdcb71b48
NisanurBulut <nbulut@sarplojistik.com>
2021-01-05 12:14:40 +0300

    implemented app.js

--- kronos/src/App.js
+++ kronos/src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
+import react from 'react';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import CountDown from './components/CountDown';
+import countDownImage from './images/countDown.jpg';
 import './App.css';
 
-const App=()=>{
-  return (
-    <div></div>
-  );
-}
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: flex,
+    flexDirection: row,
+    alignItems: center,
+    justifyContent: center,
+    height: '100vh',
+    backgroundImage:`url(${countdownImage})`,
+    backgroundPosition:center,
+    backgroundRepeat:false,
+    backgroundSize:cover
+  },
+}));
+const App = () => {
+  const classes = useStyles();
+  return <div className={classes.container}>
+    <Container maxWidth="sm">
+      <CountDown></CountDown>
+    </Container>
+  </div>;
+};
 
 export default App;
