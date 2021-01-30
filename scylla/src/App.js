696856963fe8ff333dbb04f816723c18a05a200e
NisanurBulut <nbulut@sarplojistik.com>
2021-01-30 20:02:45 +0300

    Using the useState() Hook for State Manipulation

--- scylla/src/App.js
+++ scylla/src/App.js
@@ -1,47 +1,46 @@
-import logo from './logo.svg';
 import Person from './completeGuide/Person/Person';
 import './App.css';
-import { Component } from 'react';
+import React, { useState } from 'react';
 
-class App extends Component {
-  state = {
-    persons: [
-      { name: 'Nisanur', number: 10 },
-      { name: 'Furkan', number: 11 },
-      { name: 'Yağmur', number: 12 },
-    ],
-    otherState:'some Other State'
-  };
-  switchNameHander=()=>
-  {
+// her zaman iki eleman döner
+const stateArray = useState({
+  persons: [
+    { name: 'Nisanur', number: 10 },
+    { name: 'Furkan', number: 11 },
+    { name: 'Yağmur', number: 12 },
+  ],
+  otherState: 'some Other State',
+});
+switchNameHander = () => {
   // DON'T DO THIS  this.state.persons[0].name+=' Bulut';
-  this.setState({persons:[
-    { name: 'Nisanur bulut', number: 10 },
-    { name: 'Furkan delibaş', number: 11 },
-    { name: 'Yağmur kınıcı', number: 12 },
-  ]})
-  }
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.switchNameHander}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          number={this.state.persons[0].number}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          number={this.state.persons[1].number}
-        >
-          <h2>Im doing practise on React</h2>
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          number={this.state.persons[2].number}
-        />
-      </div>
-    );
-  }
-}
+  this.setState({
+    persons: [
+      { name: 'Nisanur bulut', number: 10 },
+      { name: 'Furkan delibaş', number: 11 },
+      { name: 'Yağmur kınıcı', number: 12 },
+    ],
+  });
+};
+const App = (props) => {
+  return (
+    <div className="App">
+      <button onClick={this.switchNameHander}>Switch Name</button>
+      <Person
+        name={this.state.persons[0].name}
+        number={this.state.persons[0].number}
+      />
+      <Person
+        name={this.state.persons[1].name}
+        number={this.state.persons[1].number}
+      >
+        <h2>Im doing practise on React</h2>
+      </Person>
+      <Person
+        name={this.state.persons[2].name}
+        number={this.state.persons[2].number}
+      />
+    </div>
+  );
+};
 
 export default App;
