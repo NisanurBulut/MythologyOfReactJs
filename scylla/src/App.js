b06f60e233382d6dcea4676c5c3b46c9f9218c73
NisanurBulut <nbulut@sarplojistik.com>
2021-01-30 18:12:28 +0300

    Understanding the "children" Prop

--- scylla/src/App.js
+++ scylla/src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <Person name="nisanur" number="29"/>
+      <Person name="Nisanur" number="7">
+        <h2>Im doing practise on React</h2>
+      </Person>
     </div>
   );
 }
--- scylla/src/completeGuide/Person/Person.js
+++ scylla/src/completeGuide/Person/Person.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 
 const Person =(props) => {
-   return <h1>I'm {props.name} && I love numbers for example : {props.number}</h1>
+   return(
+   <div>
+       <h1>I'm {props.name} && I love numbers for example : {props.number}</h1>
+       <p>{props.children}</p>
+   </div>
+   );
 }
 export default Person;
