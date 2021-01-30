4c6b9b72c2aa53d2818c5db1976c74b17785c0ed
NisanurBulut <nbulut@sarplojistik.com>
2021-01-30 18:01:21 +0300

    Outputting Dynamic Content

--- scylla/src/completeGuide/Person/Person.js
+++ scylla/src/completeGuide/Person/Person.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
 const Person =() => {
-   return <h1>I'm Nisanur</h1>
+   return <h1>I'm Nisanur && I love numbers {Math.floor(Math.random()*30)}</h1>
 }
 export default Person;
