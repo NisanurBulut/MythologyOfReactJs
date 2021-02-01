374610868cd811f886e72bf89c8ad0d5f6315f9f
NisanurBulut <nbulut@sarplojistik.com>
2021-02-01 15:11:19 +0300

    Adding Styling with Stylesheets

--- scylla/src/completeGuide/Person/Person.js
+++ scylla/src/completeGuide/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import './Person.css';
 const Person =(props) => {
    return(
-   <div onClick={props.click}>
+   <div className="Person" onClick={props.click}>
        <h1>I'm {props.name} && I love numbers for example : {props.number}</h1>
        <p>{props.children}</p>
        <input type="text" onChange={props.changed} value={props.name} />
