7af22766cd63f3490215858cf2a19b7e1819668a
NisanurBulut <nbulut@sarplojistik.com>
2021-02-01 23:01:49 +0300

    implemented routing

--- myth/src/App.js
+++ myth/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Switch, Route, Redirect } from 'react-router-dom';
+import MythList from './containers/MythList';
 function App() {
   return (
     <div className="App">
-
+      <Switch>
+        <Route path={'/'} exact component={MythList} />
+        <Route path={'/myth/:myth'} exact component={MythList} />
+        <Redirect path={"/"}></Redirect>
+      </Switch>
     </div>
   );
 }
--- myth/src/containers/MythList.js
+++ myth/src/containers/MythList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-MythList=()=>{
+const MythList=()=>{
     return (
         <div>
 
