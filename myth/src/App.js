b0bfe092e5b4f5919993a27c8b97420df7ca0c4f
NisanurBulut <nbulut@sarplojistik.com>
2021-02-02 17:51:32 +0300

    refreshed style and database

--- myth/src/App.js
+++ myth/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <nav className="header">
-        <NavLink to={"/"}>Search</NavLink>
+        <NavLink to={"/"}></NavLink>
       </nav>
       <Switch>
         <Route path={'/'} exact component={MythList} />
