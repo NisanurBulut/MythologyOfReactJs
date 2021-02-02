c0a38215019e5e0fb491d8267e4eb4e8f0aa5ce8
NisanurBulut <nbulut@sarplojistik.com>
2021-02-02 14:08:24 +0300

    showed myth item detail

--- myth/src/containers/MythItem.js
+++ myth/src/containers/MythItem.js
@@ -1,10 +1,62 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { GetMyth } from '../actions/MythActions';
+import _ from 'lodash';
+
 const MythItem = (props) => {
   const mythItemName = props.match.params.myth;
-  const mythItem = useDispatch();
+  const dispathch = useDispatch();
   const mythItemState = useSelector((state) => state.MythItem);
-  console.log("myth-item",mythItemName);
-  return <div></div>;
+
+  React.useEffect(() => {
+    dispathch(GetMyth(mythItemName));
+  }, []);
+
+  const showData = () => {
+    console.log(mythItemState.data[mythItemName]);
+    if (! _.isEmpty(mythItemState.data[mythItemName])) {
+      const mythItemData=mythItemState.data[mythItemName];
+      return(
+        <div className={"myth-wrapper"}>
+          <div className={"item"}>
+            <h1>Sprites</h1>
+            <img src={mythItemData.sprites.front_default} alt=""/>
+            <img src={mythItemData.sprites.back_default} alt=""/>
+            <img src={mythItemData.sprites.front_shiny} alt=""/>
+            <img src={mythItemData.sprites.back_shiny} alt=""/>
+          </div>
+          <div className="item">
+            <h1>Stats</h1>
+            {mythItemData.stats.map(el => {
+              return <p>{el.stat.name} {el.base_stat}</p>
+            })}
+          </div>
+          <div className="item">
+            <h1>Abilities</h1>
+            {mythItemData.abilities.map(el => {
+              return <p>{el.ability.name}</p>
+            })}
+          </div>
+        </div>
+      )
+    }
+
+    if (mythItemState.loading) {
+      return <p>Loading...</p>
+    }
+
+    if (mythItemState.errorMessage !== "") {
+      return <p>{mythItemState.errorMessage}</p>
+    }
+
+    return <p>error getting myth item</p>
+  };
+
+  return (
+    <div>
+      <h1>{mythItemName}</h1>
+      {showData()}
+    </div>
+  );
 };
 export default MythItem;
