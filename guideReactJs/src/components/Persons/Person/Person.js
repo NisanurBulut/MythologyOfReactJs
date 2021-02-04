e6681d25a8fa4a6049acc7719013a09cc2c85e0b
NisanurBulut <nbulut@sarplojistik.com>
2021-02-04 21:03:02 +0300

    Component Update Lifecycle (for props

--- guideReactJs/src/components/Persons/Person/Person.js
+++ guideReactJs/src/components/Persons/Person/Person.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, {Component} from 'react'
 import StyledPerson from './Person.css';
 
-const Person =(props) => {
-   return(
-   <div className={StyledPerson.Person}>
-       <h1 onClick={props.click}>I'm {props.name} && I love numbers for example : {props.number}</h1>
-       <p>{props.children}</p>
-       <input type="text" onChange={props.changed} value={props.name} />
-   </div>
-   );
+class Person extends Component {
+    constructor(props){
+        super(props);
+    }
+    render (){
+        return(
+        <div className={StyledPerson.Person}>
+            <h1 onClick={this.props.click}>I'm {this.props.name}
+            && I love numbers for example : {this.props.number}</h1>
+            <p>{this.props.children}</p>
+            <input type="text"
+            onChange={this.props.changed} value={this.props.name} />
+        </div>
+        );
+    }
 }
 export default Person;
--- guideReactJs/src/components/Persons/Persons.js
+++ guideReactJs/src/components/Persons/Persons.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import Person  from './Person/Person';
-import styled from './Persons.css'
-const Persons = (props) => props.persons.map((person, index) => {
-    return (<div className={styled.Persons}>
-        <Person
-        key={person.id}
-        name={person.name}
-        number={person.number}
-        click={() => props.deleteClicked(index)}
-        changed={(event) =>
-          props.userNameChanged(event, person.id)}
-      />
-    </div>)
-  });
-  export default Persons;
+import React, { Component } from 'react';
+import Person from './Person/Person';
+import styled from './Persons.css';
+class Persons extends Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return this.props.persons.map((person, index) => {
+      return (
+        <div className={styled.Persons}>
+          <Person
+            key={person.id}
+            name={person.name}
+            number={person.number}
+            click={() => this.props.deleteClicked(index)}
+            changed={(event) => this.props.userNameChanged(event, person.id)}
+          />
+        </div>
+      );
+    });
+  }
+}
+export default Persons;
