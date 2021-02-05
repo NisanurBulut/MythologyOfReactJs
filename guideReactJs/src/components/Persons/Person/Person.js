0745b4f112e6a853980e55950d981340ba0500b8
NisanurBulut <nbulut@sarplojistik.com>
2021-02-05 10:53:16 +0300

    Rendering Adjacent JSX Elements

--- guideReactJs/src/components/Persons/Person/Person.js
+++ guideReactJs/src/components/Persons/Person/Person.js
@@ -1,19 +1,26 @@
 import React, {Component} from 'react'
 import StyledPerson from './Person.css';
-
+import Auxiliary from '../../../hoc/Auxiliary';
 class Person extends Component {
     constructor(props){
         super(props);
     }
     render (){
         return(
-        <div className={StyledPerson.Person}>
-            <h1 onClick={this.props.click}>I'm {this.props.name}
+            <Auxiliary>
+                <h1 onClick={this.props.click}>I'm {this.props.name}
             && I love numbers for example : {this.props.number}</h1>
             <p>{this.props.children}</p>
             <input type="text"
             onChange={this.props.changed} value={this.props.name} />
-        </div>
+            </Auxiliary>
+        // <div className={StyledPerson.Person}>
+        //     <h1 onClick={this.props.click}>I'm {this.props.name}
+        //     && I love numbers for example : {this.props.number}</h1>
+        //     <p>{this.props.children}</p>
+        //     <input type="text"
+        //     onChange={this.props.changed} value={this.props.name} />
+        // </div>
         );
     }
 }
---
+++ guideReactJs/src/hoc/Auxiliary.js
@@ -0,0 +1,4 @@
+import { React } from 'react';
+const auxiliary = (props) => props.children;
+
+export default auxiliary;
