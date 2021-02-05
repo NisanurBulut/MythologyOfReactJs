9a59de362a4d337373b06f6703a338412c7e2289
NisanurBulut <nbulut@sarplojistik.com>
2021-02-05 18:47:06 +0300

    Higher Order Components (HOC) - Introduction

--- guideReactJs/src/containers/App.js
+++ guideReactJs/src/containers/App.js
@@ -1,7 +1,7 @@
 import AppCompleteGuide from './AppCompleteGuide/AppCompleteGuide';
 import classes from '../containers/App.css';
 import React, { PureComponent } from 'react';
-import WithClass from '../../hoc/WithClass';
+import WithClass from '../hoc/WithClass';
 
 class App extends PureComponent {
   render() {
