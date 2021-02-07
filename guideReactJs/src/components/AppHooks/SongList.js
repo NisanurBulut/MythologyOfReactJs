44aa4b2ceb016c3efbe857a4ff107d2d0bd939a4
NisanurBulut <nbulut@sarplojistik.com>
2021-02-07 13:34:25 +0300

    implemented AppHooks

---
+++ guideReactJs/src/components/AppHooks/SongList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+const SongList = () => {
+    return ( <div className="songList">
+        <ul>
+            <li>I will Survive</li>
+        </ul>
+    </div> );
+}
+
+export default SongList;
---
+++ guideReactJs/src/containers/AppHooks/AppHooks.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import SongList from '../../components/AppHooks/SongList';
+class AppHooks extends Component {
+  render() {
+    return (
+        <SongList></SongList>
+    );
+  }
+}
+
+export default AppHooks;
