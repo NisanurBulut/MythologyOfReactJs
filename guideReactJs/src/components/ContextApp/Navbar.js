7a023122888f2f638acd4bdc6843eee2b6fc864b
NisanurBulut <nbulut@sarplojistik.com>
2021-02-07 15:06:31 +0300

    Multiple Contexts using Hooks

--- guideReactJs/src/components/ContextApp/Navbar.js
+++ guideReactJs/src/components/ContextApp/Navbar.js
@@ -1,36 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import classes from '../../containers/AppContextApi/AppContextApi.css';
 import { ThemeContext } from '../../containers/AppContextApi/contexts/ThemeContext';
 import { AuthContext } from '../../containers/AppContextApi/contexts/AuthContext';
-class Navbar extends Component {
-  render() {
-    // type olarak consume ettik ancak okurken context kullanıdğımıza dikkat et
-    return (
-      // consume to themecontext
-      // bu şekilde fonksiyonel olarak erişmenin avantajı  multiple context tanımları olduğunda erişimin mümkün olmasıdır
-      <AuthContext.Consumer>{(authContext) => {
-          <ThemeContext.Consumer>{(themeContext) => {
-              const { isAuthenticated, toggleAuth } = authContext;
-              const { isLightTheme, light, dark } = themeContext;
-              const theme = isLightTheme ? light : dark;
-              return (
-                <nav
-                  className={classes.nav}
-                  style={{ background: theme.ui, color: theme.style }}
-                >
-                  <h1>Context App</h1>
-                  <div onClick={toggleAuth}>{isAuthenticated ? 'Logged in' : 'Log out'}</div>
-                  <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                  </ul>
-                </nav>
-              );
-            }}</ThemeContext.Consumer>;
-        }}</AuthContext.Consumer>
-    );
-  }
+
+const Navbar = () => {
+  const { isLightTheme, light, dark }=useContext(ThemeContext); // destructing
+  const theme = isLightTheme ? light : dark;
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  return (
+    <nav
+      className={classes.nav}
+      style={{ background: theme.ui, color: theme.style }}
+    >
+      <h1>Context App</h1>
+      <div onClick={toggleAuth}>{isAuthenticated ? 'Logged in' : 'Logged out'}</div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
