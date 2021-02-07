745f69ac78d4acafe56fcf01c6dd924928169fd9
NisanurBulut <nbulut@sarplojistik.com>
2021-02-07 12:44:44 +0300

    functional context api for consume

--- guideReactJs/src/components/ContextApp/Navbar.js
+++ guideReactJs/src/components/ContextApp/Navbar.js
@@ -2,24 +2,30 @@ import React, { Component } from 'react';
 import classes from '../../containers/AppContextApi/AppContextApi.css';
 import { ThemeContext } from '../../containers/AppContextApi/contexts/ThemeContext';
 class Navbar extends Component {
-  // consume to themecontext
-  static contextType = ThemeContext;
   render() {
-    console.log(this.context); // type olarak consume ettik ancak okurken context kullanıdğımıza dikkat et
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
+    // type olarak consume ettik ancak okurken context kullanıdğımıza dikkat et
     return (
-      <nav
-        className={classes.nav}
-        style={{ background: theme.ui, color: theme.style }}
-      >
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      // consume to themecontext
+      // bu şekilde fonksiyonel olarak erişmenin avantajı  multiple context tanımları olduğunda erişimin mümkün olmasıdır
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav
+              className={classes.nav}
+              style={{ background: theme.ui, color: theme.style }}
+            >
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
