a893004eddabd4cf42b3de08c329df7e964c3f3f
NisanurBulut <nbulut@sarplojistik.com>
2021-02-07 15:08:19 +0300

    Multiple Contexts using Hooks

--- guideReactJs/src/components/ContextApp/ThemeToggle.js
+++ guideReactJs/src/components/ContextApp/ThemeToggle.js
@@ -1,12 +1,9 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../../containers/AppContextApi/contexts/ThemeContext';
 
-class ThemeToggle extends Component {
-  static contextType = ThemeContext; // burada erişimi sağlayabilmek için bu keywordler kullanılmak zorunda
-  render() {
-    const { toggleTheme } = this.context; // context içerisinden adı toggleTheme olan değişkeni al diyoruz burada
-    return <button onClick={toggleTheme}>Toggle Theme</button>;
-  }
+const ThemeToggle = () => {
+  const {toggleTheme} = useContext(ThemeContext)
+  return <button onClick={toggleTheme}>Toggle Theme</button>;
 }
 
 export default ThemeToggle;
