d95e8caff56e4c8564b245629a949b141406a1fb
NisanurBulut <nbulut@sarplojistik.com>
2021-02-07 13:19:04 +0300

    consume multiple context

--- guideReactJs/src/components/ContextApp/Navbar.js
+++ guideReactJs/src/components/ContextApp/Navbar.js
@@ -1,31 +1,34 @@
 import React, { Component } from 'react';
 import classes from '../../containers/AppContextApi/AppContextApi.css';
 import { ThemeContext } from '../../containers/AppContextApi/contexts/ThemeContext';
+import { AuthContext } from '../../containers/AppContextApi/contexts/AuthContext';
 class Navbar extends Component {
   render() {
     // type olarak consume ettik ancak okurken context kullanıdğımıza dikkat et
     return (
       // consume to themecontext
       // bu şekilde fonksiyonel olarak erişmenin avantajı  multiple context tanımları olduğunda erişimin mümkün olmasıdır
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <nav
-              className={classes.nav}
-              style={{ background: theme.ui, color: theme.style }}
-            >
-              <h1>Context App</h1>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-          );
-        }}
-      </ThemeContext.Consumer>
+      <AuthContext.Consumer>{(authContext) => {
+          <ThemeContext.Consumer>{(themeContext) => {
+              const { isAuthenticated, toggleAuth } = authContext;
+              const { isLightTheme, light, dark } = themeContext;
+              const theme = isLightTheme ? light : dark;
+              return (
+                <nav
+                  className={classes.nav}
+                  style={{ background: theme.ui, color: theme.style }}
+                >
+                  <h1>Context App</h1>
+                  <div onClick={toggleAuth}>{isAuthenticated ? 'Logged in' : 'Log out'}</div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+              );
+            }}</ThemeContext.Consumer>;
+        }}</AuthContext.Consumer>
     );
   }
 }
--- guideReactJs/src/containers/AppContextApi/AppContextApi.js
+++ guideReactJs/src/containers/AppContextApi/AppContextApi.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import Navbar from '../../components/ContextApp/Navbar';
 import BookList from '../../components/ContextApp/BookList';
 import ThemeContextProvider from './contexts/ThemeContext';
-import AuthontextProvider from './contexts/AuthContext';
+import AuthContextProvider from './contexts/AuthContext';
 import classes from './AppContextApi.css';
 import ThemeToggle from '../../components/ContextApp/ThemeToggle';
 
@@ -10,13 +10,13 @@ class AppContextApi extends PureComponent {
   render() {
     return (
       <ThemeContextProvider>
-        <AuthontextProvider>
+        <AuthContextProvider>
           <div className={classes.main}>
             <Navbar></Navbar>
             <BookList></BookList>
             <ThemeToggle />
           </div>
-        </AuthontextProvider>
+        </AuthContextProvider>
       </ThemeContextProvider>
     );
   }
