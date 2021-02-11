1b550f574f758e5bfdb391cce5d2fdd683033418
NisanurBulut <nbulut@sarplojistik.com>
2021-02-11 14:05:29 +0300

    defined PropTypes

--- eros/src/components/Header.js
+++ eros/src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 const Header =({title})=> {
         return (
@@ -9,5 +10,9 @@ const Header =({title})=> {
     }
 Header.defaultProps = {
     title:'Love Tracker'
+}
+
+Header.PropTypes={
+    title:PropTypes.string
 }
 export default Header;
