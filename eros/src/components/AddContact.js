35881592310eaab1fec2b56d4963de1106a684f2
NisanurBulut <nbulut@sarplojistik.com>
2021-02-11 16:17:10 +0300

    implemented usestate for add-form

--- eros/src/components/AddContact.js
+++ eros/src/components/AddContact.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function AddContact() {
+  const [text, setText] = useState('');
+  const [day, setDay] = useState('');
   return (
     <form className="add-form">
       <div className="form-control">
-        <input type="text" placeholder="contact"></input>{' '}
+        <input
+          type="text"
+          placeholder="contact"
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
       <div className="form-control">
-        <input type="text" placeholder="birthDay"></input>{' '}
+        <input
+          type="text"
+          placeholder="birthDay"
+          onChange={(e) => setDay(e.target.value)}
+        />
       </div>
-      <input type="submit" value="save" className='btn btn-block' />
+      <input type="submit" value="save" className="btn btn-block" />
     </form>
   );
 }
