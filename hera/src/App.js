eac4c6d8373407fe0b620698e628149f5e6ccaa3
NisanurBulut <nbulut@sarplojistik.com>
2021-03-20 19:44:52 +0300

    listed birthdays from mongo db via graphql

--- hera/src/App.js
+++ hera/src/App.js
@@ -2,18 +2,25 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 import axios from 'axios';
+import { GET_BIRTHDAYS } from './util/queries';
 
 function App() {
-  const [people,setPeople]=useState(data);
+  const [people, setPeople] = useState([]);
+  const getBirthdays = () => {
+    axios
+      .post('http://localhost:4000/graphql', GET_BIRTHDAYS)
+      .then((res) => setPeople(res.data.data.getBirthdays))
+      .catch((err) => console.log(err));
+  };
   return (
     <main>
-    <section className="container">
-      <h3>{people.length} Doğum Günü hatırlatması </h3>
-      <List people={people}></List>
-      <button onClick={()=>setPeople([])}>Hatırlatıcıyı Temizle</button>
-    </section>
-  </main>
-  )
+      <section className="container">
+        <h3>{people.length} Doğum Günü hatırlatması </h3>
+        <List people={people}></List>
+        <button onClick={getBirthdays}>Listele</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
